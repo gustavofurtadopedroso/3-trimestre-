7923beaf01c58660239afee0fbf9c899d219bc36
gustavofurtadopedroso <150378625+gustavofurtadopedroso@users.noreply.github.com>
2024-10-01 14:45:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Onde o CSS é utilizado?',
+    'O CSS é usado para estilizar o conteúdo HTML, definindo cores, fontes, espaçamentos, layouts e outros aspectos visuais.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o páis mais populoso no mundo?',
+    'O país mais populoso é a India'
+)
+
+criaCartao(
+    'Programação',
+    'Onde é utilizado o HTML',
+    'desenvolvimento frontend'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz eu em Inglês?',
+    'EU em ingles é I  (RAI)'
+)
